fcc3ebf538beaf9d940851becaf88146f4a6abc6
Pablo Hernán Rodriguez Zivic <elsonidoq@gmail.com>
2016-12-22 00:04:38 -0300

    improved the use of fito for caching

--- download.py
+++ download.py
@@ -6,19 +6,18 @@ from datetime import datetime, timedelta
 import pymongo
 from rabbit_mq import RabbtMQ
 from fito.data_store.mongo import MongoHashMap
-from fito.operations.decorate import as_operation
 
 cache = MongoHashMap(pymongo.MongoClient().temperatures.by_hour)
 
 
-@cache.autosave
-@as_operation()
-def get_for_date(day, month, year):
-    url_template = 'http://www.meteored.com.ar/peticiones/datosgrafica_sactual_16.php?id_estacion=571e07bdc76c49177837d604&accion=T&id_localidad=13584&anno={year}&mes={month}&dia={day}'
+@cache.cache()
+def get_for_date(station_id, day, month, year):
+    url_template = 'http://www.meteored.com.ar/peticiones/datosgrafica_sactual_16.php?id_estacion={station_id}&accion=T&id_localidad=13584&anno={year}&mes={month}&dia={day}'
     url = url_template.format(
         month=month,
         day=day,
         year=year,
+        station_id=station_id,
     )
     response = urlopen(url).read()
     if response: return eval(response)
@@ -39,14 +38,24 @@ def main():
 
     elif args.mode == 'push':
         d = datetime.now()
+        available_stations = [
+            # '571e07bcc76c49177837d5d4',
+            # '571e07bdc76c49177837d604',
+            # '571e07bcc76c49177837d5e9',
+            # '571e07bcc76c49177837d5e6',
+            '571e079bc76c49177837cd9b',
+
+        ]
         with RabbtMQ().connection() as connection:
             for i in xrange(20 * 365):
-                d -= timedelta(days=1)
-                connection.push_socket('get_temperatures', {
-                    'day': d.day,
-                    'month': d.month,
-                    'year': d.year,
-                })
+                for station_id in available_stations:
+                    d -= timedelta(days=1)
+                    connection.push_socket('get_temperatures', {
+                        'day': d.day,
+                        'month': d.month,
+                        'year': d.year,
+                        'station_id': station_id,
+                    })
     else:
         with RabbtMQ().connection() as connection:
             connection.channel.queue_purge('get_temperatures')
